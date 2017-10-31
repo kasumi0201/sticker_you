05df05f6fe0ac8bc36a40195d7d4c7a0d30c9d48
kasumi <kasumi0201@gmail.com>
2017-10-31 16:40:58 +1100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+   root 'home#index'
+  devise_for :users, controllers: {
+       sessions: 'users/sessions'
+     }
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
